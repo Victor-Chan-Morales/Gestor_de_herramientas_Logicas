ee0fd4c326861d647edd15894228345b05c4047e
rtdash <gonz.deron.gt@gmail.com>
2025-10-27 22:39:00 -0600

    Fix: Desactivar service worker y configurar rutas absolutas para Flutter en GitHub Pages - Agregar .nojekyll y ajustar assetBase

--- evaluador-regex/flutter_bootstrap.js
+++ evaluador-regex/flutter_bootstrap.js
@@ -37,7 +37,8 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded610","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "1655665703"
+  config: {
+    // Configurar rutas para GitHub Pages
+    assetBase: "/Gestor_de_herramientas_Logicas/evaluador-regex/"
   }
 });
